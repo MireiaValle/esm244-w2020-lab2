82f4e82677ae09c10eb4bacb54f345646cb7aded
MireiaValle <mireia.valle@bc3research.org>
2020-01-16 01:14:30 -0800

    lab2 script

---
+++ lab2.R
@@ -0,0 +1,18 @@
+#Install packages
+
+install.packages ("naniar")
+install.packages ("VIM")
+install.packages ("skimr")
+install.packages ("ggfortify")
+install.packages ("shinythemes")
+
+#load libraries
+
+library(naniar)
+library(VIM)
+library(skimr)
+library(ggfortify)
+library(shinythemes)
+
+
+
